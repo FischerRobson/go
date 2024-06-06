2c396ac187e3ce81d952e0dd1fda35082c27b453
Robson Fischer <fischerrobson@gmail.com>
2024-06-05 21:31:32 -0300

    interfaces in go

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,13 +1,34 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct {
 }
 
 type spanishBot struct {
 }
 
-
-
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// can ommit instance if not uses
+func (englishBot) getGreeting(text string, n int) (string, err) {
+	return fmt.Sprintf("Hello %s, %i", text, n), nil
+}
+
+func (sp spanishBot) getGreeting(text string, n int) (string, err) {
+	return fmt.Sprintf("Hola %s, %i", text, n), nil
 }
